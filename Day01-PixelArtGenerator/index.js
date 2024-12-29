2490cfe4a36d2ac04ffc229ae8d6a13a12d62b2c
tomhoon <gnsdl9108@naver.com>
2024-12-29 19:23:00 +0900

    day1

---
+++ Day01-PixelArtGenerator/index.js
@@ -0,0 +1,9 @@
+let rangeWidth = document.querySelector(".range-width");
+let rangeHeight = document.querySelector(".range-height");
+
+let createBtn = document.querySelector(".create-btn");
+let clearBtn = document.querySelector(".clear-btn");
+let colorBtn = document.querySelector("#color-btn");
+let eraseBtn = document.querySelector(".erase-btn");
+let paintBtn = document.querySelector(".paint-btn");
+
